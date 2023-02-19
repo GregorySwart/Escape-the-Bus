c940bb7a6308f2d33df07e05efdff1c72278b2a7
gregoryswart <gregory.swart97@gmail.com>
2023-02-19 16:29:31 +0000

    Refactor

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 from models import PlayingCard, CardSuit, GameStrategy
-from typing import List, Tuple
+from typing import List, Tuple, Optional
 from random import shuffle, randint
 
 
 class Game:
     cards: List[PlayingCard] = []
+    strategy: GameStrategy = GameStrategy.SENSIBLE
 
     def __init__(self):
         for suit in range(4):
             for number in range(13):
                 self.cards.append(PlayingCard(number + 1, suit + 1))
+        self.strategy = GameStrategy.SENSIBLE
 
     def deal_new_deck(self):
         self.cards = []
@@ -24,8 +26,8 @@ class Game:
         drawn_card = self.cards.pop(0)
         return drawn_card
 
-    def play_first_card(self, strategy: GameStrategy) -> Tuple[PlayingCard, str, bool]:
-        if strategy in [GameStrategy.RANDOM, GameStrategy.SENSIBLE]:
+    def play_first_card(self) -> Tuple[PlayingCard, str, bool]:
+        if self.strategy in [GameStrategy.RANDOM, GameStrategy.SENSIBLE]:
             guess = ["RED", "BLACK"][randint(0, 1)]
         else:
             n_red = len([c for c in self.cards if c.colour() == "RED"])
@@ -40,10 +42,10 @@ class Game:
 
         return first_card, guess, success
 
-    def play_second_card(self, first_card: PlayingCard, strategy: GameStrategy) -> Tuple[PlayingCard, bool, bool]:
-        if strategy == GameStrategy.RANDOM:
+    def play_second_card(self, first_card: PlayingCard) -> Tuple[PlayingCard, bool, bool]:
+        if self.strategy == GameStrategy.RANDOM:
             guess = bool(randint(0, 1))
-        elif strategy == GameStrategy.SENSIBLE:
+        elif self.strategy == GameStrategy.SENSIBLE:
             if first_card.num_int() < 7:
                 guess = True
             elif first_card.num_int() > 7:
@@ -71,17 +73,16 @@ class Game:
 
         return second_card, guess, success
 
-    def play_third_card(self, first_card: PlayingCard, second_card: PlayingCard,
-                        strategy: GameStrategy) -> Tuple[PlayingCard, bool, bool]:
+    def play_third_card(self, first_card: PlayingCard, second_card: PlayingCard) -> Tuple[PlayingCard, bool, bool]:
         if first_card.number.value < second_card.number.value:
             span_with_edges = list(range(first_card.number.value, second_card.number.value))
         else:
             span_with_edges = list(range(second_card.number.value, first_card.number.value))
         span = span_with_edges[1:-1]
 
-        if strategy == GameStrategy.RANDOM:
+        if self.strategy == GameStrategy.RANDOM:
             guess = bool(randint(0, 1))
-        elif strategy == GameStrategy.SENSIBLE:
+        elif self.strategy == GameStrategy.SENSIBLE:
             guess = True if abs(first_card.number.value - second_card.number.value) > 6 else False
         else:
             n_within = len([c for c in self.cards if c.number.value in span])
@@ -98,8 +99,8 @@ class Game:
 
         return third_card, guess, success
 
-    def play_fourth_card(self, strategy: GameStrategy.RANDOM) -> Tuple[PlayingCard, bool, bool]:
-        if strategy in [GameStrategy.RANDOM, GameStrategy.SENSIBLE]:
+    def play_fourth_card(self) -> Tuple[PlayingCard, bool, bool]:
+        if self.strategy in [GameStrategy.RANDOM, GameStrategy.SENSIBLE]:
             guess = CardSuit(randint(1, 4))
         else:
             card_suits = [c.suit for c in self.cards]
@@ -113,6 +114,39 @@ class Game:
         success = guess == fourth_card.suit
 
         return fourth_card, guess, success
+
+    def play(self, strategy: Optional[GameStrategy] = None) -> Tuple[bool, int]:
+        self.deal_new_deck()
+        self.shuffle_deck()
+
+        if strategy:
+            self.strategy = strategy
+
+        escape = False
+        while not escape:
+            try:
+                first_card, guess, success_first = self.play_first_card()
+                if not success_first:
+                    continue
+
+                second_card, guess, success_second = self.play_second_card(first_card)
+                if not success_second:
+                    continue
+
+                third_card, guess, success_third = self.play_third_card(first_card, second_card)
+                if not success_third:
+                    continue
+
+                fourth_card, guess, success_fourth = self.play_fourth_card()
+                if not success_fourth:
+                    continue
+
+                escape = True
+
+            except IndexError:
+                return False, 0
+
+        return True, len(self.cards)
 
     def play_random(self) -> Tuple[bool, int]:
         self.deal_new_deck()
@@ -144,74 +178,15 @@ class Game:
 
         return True, len(self.cards)
 
-    def play_sensible(self) -> Tuple[bool, int]:
-        self.deal_new_deck()
-        self.shuffle_deck()
-        escape = False
-
-        while not escape:
-            try:
-                first_card, guess, success_first = self.play_first_card(GameStrategy.SENSIBLE)
-                if not success_first:
-                    continue
-
-                second_card, guess, success_second = self.play_second_card(first_card, GameStrategy.SENSIBLE)
-                if not success_second:
-                    continue
-
-                third_card, guess, success_third = self.play_third_card(first_card, second_card, GameStrategy.SENSIBLE)
-                if not success_third:
-                    continue
-
-                fourth_card, guess, success_fourth = self.play_fourth_card(GameStrategy.SENSIBLE)
-                if not success_fourth:
-                    continue
-
-                escape = True
-
-            except IndexError:
-                return False, 0
-
-        return True, len(self.cards)
-
-    def play_optimal(self) -> Tuple[bool, int]:
-        self.deal_new_deck()
-        self.shuffle_deck()
-        escape = False
-
-        while not escape:
-            try:
-                first_card, guess, success_first = self.play_first_card(GameStrategy.OPTIMISED)
-                if not success_first:
-                    continue
-
-                second_card, guess, success_second = self.play_second_card(first_card, GameStrategy.OPTIMISED)
-                if not success_second:
-                    continue
-
-                third_card, guess, success_third = self.play_third_card(first_card, second_card, GameStrategy.OPTIMISED)
-                if not success_third:
-                    continue
-
-                fourth_card, guess, success_fourth = self.play_fourth_card(GameStrategy.OPTIMISED)
-                if not success_fourth:
-                    continue
-
-                escape = True
-
-            except IndexError:
-                return False, 0
-
-        return True, len(self.cards)
-
 
 def main():
     new_game = Game()
+    new_game.strategy = GameStrategy.OPTIMISED
     outcomes = []
     card_counter = []
     for i in range(100):
         print(f"Playing game {i + 1}...")
-        outcome, n_cards = new_game.play_optimal()
+        outcome, n_cards = new_game.play()
         outcomes.append(outcome)
         card_counter.append(n_cards)
         # print(n_cards)
