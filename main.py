28f16053e39c1c40c12519eef74bed61bd1df4da
gregoryswart <gregory.swart97@gmail.com>
2023-02-19 11:42:09 +0000

    Refactored guessing methods

--- main.py
+++ main.py
@@ -24,9 +24,12 @@ class Game:
         drawn_card = self.cards.pop(0)
         return drawn_card
 
-    # Strategy 1: Random
     def play_first_card(self, strategy: GameStrategy) -> Tuple[PlayingCard, str, bool]:
-        guess = ["RED", "BLACK"][randint(0, 1)]
+        if strategy in [GameStrategy.RANDOM, GameStrategy.SENSIBLE]:
+            guess = ["RED", "BLACK"][randint(0, 1)]
+        else:
+            # TODO Implement Optimised play
+            guess = ["RED", "BLACK"][randint(0, 1)]
         first_card = self.draw_card()
         success = guess == first_card.suit.colour()
 
@@ -59,75 +62,36 @@ class Game:
 
         return second_card, guess, success
 
-    def play_second_card_random(self, first_card: PlayingCard) -> Tuple[PlayingCard, bool, bool]:
-        second_card = self.draw_card()
-        guess = bool(randint(0, 1))
-        if second_card.number.value == first_card.number.value:
-            is_higher = None
-        elif second_card.number.value > first_card.number.value:
-            is_higher = True
+    def play_third_card(self, first_card: PlayingCard, second_card: PlayingCard,
+                        strategy: GameStrategy) -> Tuple[PlayingCard, bool, bool]:
+        if strategy == GameStrategy.RANDOM:
+            guess = bool(randint(0, 1))
+        elif strategy == GameStrategy.SENSIBLE:
+            guess = True if abs(first_card.number.value - second_card.number.value) > 6 else False
         else:
-            is_higher = False
-        success = guess == is_higher
+            # TODO Implement Optimised play
+            guess = bool(randint(0, 1))
 
-        return second_card, guess, success
-
-    def play_third_card_random(
-            self, first_card: PlayingCard, second_card: PlayingCard) -> Tuple[PlayingCard, bool, bool]:
         third_card = self.draw_card()
         if first_card.number.value < second_card.number.value:
             span = list(range(first_card.number.value, second_card.number.value))
         else:
             span = list(range(second_card.number.value, first_card.number.value))
         is_inside = third_card.number.value in span
-        if third_card.number.value in [first_card.number.value, second_card.number.value]:
-            is_inside = None
-        guess = bool(randint(0, 1))
         success = guess == is_inside
 
         return third_card, guess, success
 
-    def play_fourth_card(self) -> Tuple[PlayingCard, bool, bool]:
+    def play_fourth_card(self, strategy: GameStrategy.RANDOM) -> Tuple[PlayingCard, bool, bool]:
+        if strategy in [GameStrategy.RANDOM, GameStrategy.SENSIBLE]:
+            guess = CardSuit(randint(1, 4))
+        else:
+            # TODO Implement Optimised play
+            guess = CardSuit(randint(1, 4))
         fourth_card = self.draw_card()
-        guess = CardSuit(randint(1, 4))
         success = guess == fourth_card.suit
 
         return fourth_card, guess, success
-
-    # Strategy 2: Sensible
-    def play_second_card_sensible(self, first_card: PlayingCard):
-        # The question is "Is the second card higher than the first?", so guess == True corresponds to a higher card
-        if first_card.number.value < 7:
-            guess = True
-        elif first_card.number.value > 7:
-            guess = False
-        else:
-            guess = bool(randint(0, 1))
-
-        second_card = self.draw_card()
-        if second_card.number.value == first_card.number.value:
-            is_higher = None
-        elif second_card.number.value > first_card.number.value:
-            is_higher = True
-        else:
-            is_higher = False
-
-        success = guess == is_higher
-        return second_card, guess, success
-
-    def play_third_card_sensible(
-            self, first_card: PlayingCard, second_card: PlayingCard) -> Tuple[PlayingCard, bool, bool]:
-        guess = True if abs(first_card.number.value - second_card.number.value) > 6 else False
-        third_card = self.draw_card()
-
-        if first_card.number.value < second_card.number.value:
-            span = list(range(first_card.number.value, second_card.number.value))
-        else:
-            span = list(range(second_card.number.value, first_card.number.value))
-        is_inside = third_card.number.value in span
-        success = guess == is_inside
-
-        return third_card, guess, success
 
     def play_random(self) -> Tuple[bool, int]:
         self.deal_new_deck()
@@ -144,11 +108,11 @@ class Game:
                 if not success_second:
                     continue
 
-                third_card, guess, success_third = self.play_second_card_random(first_card)
+                third_card, guess, success_third = self.play_third_card(first_card, second_card, GameStrategy.RANDOM)
                 if not success_third:
                     continue
 
-                fourth_card, guess, success_fourth = self.play_fourth_card()
+                fourth_card, guess, success_fourth = self.play_fourth_card(GameStrategy.RANDOM)
                 if not success_fourth:
                     continue
 
@@ -174,11 +138,11 @@ class Game:
                 if not success_second:
                     continue
 
-                third_card, guess, success_third = self.play_second_card_sensible(first_card)
+                third_card, guess, success_third = self.play_third_card(first_card, second_card, GameStrategy.SENSIBLE)
                 if not success_third:
                     continue
 
-                fourth_card, guess, success_fourth = self.play_fourth_card()
+                fourth_card, guess, success_fourth = self.play_fourth_card(GameStrategy.SENSIBLE)
                 if not success_fourth:
                     continue
 
